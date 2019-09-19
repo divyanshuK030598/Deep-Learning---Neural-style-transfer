6712e98b321cedf94a7cb1c1984dd7f08cb3f9e9
Divyanshu Kumawat <55530450+divyanshuK030598@users.noreply.github.com>
2019-09-19 15:27:29 +0530

    Create style_matrix.py

---
+++ style_matrix.py
@@ -0,0 +1,14 @@
+def gram_matrix(A):
+    """
+    Argument:
+    A -- matrix of shape (n_C, n_H*n_W)
+    
+    Returns:
+    GA -- Gram matrix of A, of shape (n_C, n_C)
+    """
+    
+   
+    GA = tf.matmul(A, tf.transpose(A))
+
+    
+    return GA
